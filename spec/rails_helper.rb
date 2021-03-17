916f8940728c1f3dc4b9f0fc0c2f000b1766c47a
João <victorpow@live.com>
2021-03-17 00:00:02 -0300

    added some validation for user and config shoulda

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,11 +10,19 @@ SimpleCov.start 'rails' do
   add_filter '/app/mailers'
 end
 
+
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -44,7 +52,7 @@ RSpec.configure do |config|
   config.include Warden::Test::Helpers
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
+  
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
