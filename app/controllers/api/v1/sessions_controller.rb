1ab6fbd0f2bc2fd2a0c3cde4993178016dd251b4
João <victorpow@live.com>
2021-03-18 03:47:28 -0300

    responding with the devise jwt token in the body

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,13 +1,21 @@
 class Api::V1::SessionsController < Devise::SessionsController
   respond_to :json
 
+  def create
+    super { @token = current_token }
+  end
+
   private
 
   def respond_with(resource, _opts = {})
-    render json: resource
+    render json: "access token: { Bearer #{@token} }"
   end
 
   def respond_to_on_destroy
     head :no_content
+  end
+
+  def current_token
+    request.env['warden-jwt_auth.token']
   end
 end
